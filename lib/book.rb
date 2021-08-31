21c82f9530cf5e468d7a5c67b9751544a29bef84
alec <kaltschmidalec@gmail.com>
2021-08-31 10:29:23 -0700

    commit tree a380536af60124b336506435db89d1f0e4908b48

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
+
+    def turn_page
+        print "Flipping the page...wow, you read fast!"
+    end
+end
 
